9d7c38891584cfc28cdb98946dfbba277f545876
Mohamed Maskoul <66232435+maskoul-mohamed@users.noreply.github.com>
2022-02-01 11:16:01 +0000

    Add OOP example 4

---
+++ 12-OOP/objects and classes/example4.php
@@ -0,0 +1,29 @@
+<?php
+    class BankAccount {
+        public $accountNumber;
+        public $balance;
+
+        public function deposit($amount){
+            if($amount> 0){
+                $this->$balance += $amount;
+            }
+        }
+
+        public function withdraw($amount){
+            if($amount<= $this->$balance) {
+                $this->balance -= $amount;
+                return true;
+            }
+            return false;
+        }
+    }
+
+    $account = new BankAccount;
+
+    $account->$accountNumber = 1;
+    $account->$balance = 100;
+
+    $respons =  $account->withdraw(122);
+
+    echo var_dump($respons);
+?>
