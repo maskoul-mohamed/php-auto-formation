a5ae14779be04563e701fc8017432fec3f6f13a6
Mohamed Maskoul <66232435+maskoul-mohamed@users.noreply.github.com>
2022-02-03 13:44:17 +0000

    Delete print type of post

--- 11-forms/orgonized form/inc/post.php
+++ 11-forms/orgonized form/inc/post.php
@@ -1,6 +1,5 @@
 <?php
     if(isset($_POST['name'], $_POST['email'])){
-        echo var_dump($_POST);
 
         $name = htmlspecialchars($_POST['name']);
         $email = htmlspecialchars($_POST['email']);
